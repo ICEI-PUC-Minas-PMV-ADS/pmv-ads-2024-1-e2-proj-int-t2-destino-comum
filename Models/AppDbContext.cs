cefce046bca77d7359d4ef8a531704263a6c6f30
Mariana Dias <marianamkt6@gmail.com>
2024-05-08 20:51:52 -0300

    configurando o acesso as paginas

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using DestinoComum2.Dto.Usuario;
 
 namespace DestinoComum2.Models
 {
@@ -8,6 +9,8 @@ namespace DestinoComum2.Models
 
         public DbSet<CidadeModel> Cidades { get; set; }
         public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<DestinoComum2.Dto.Usuario.UsuarioCriacaoDto> UsuarioCriacaoDto { get; set; }
+        public DbSet<DestinoComum2.Dto.Usuario.UsuarioEditarDto> UsuarioEditarDto { get; set; }
         //public DbSet<ComentarioModel> Comentarios { get; set; }
 
 
